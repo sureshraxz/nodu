c0d9964e4e9b0cd71af10eac494a70f77bea8de0
sureshraxz <sureshraxz@gmail.comm>
2021-08-19 07:48:54 +0530

    Basic api call created in express and tested using postman

---
+++ express-basics/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+//start up the server
+app.listen(3000, () =>{
+    console.log('App running on port 3000');
+});
+
+//route setup
+//get method - can be tested in browser + postman - 127.0.0.1:3000/
+app.get('/', (req,res) => {
+    res.status(200).send('Hello from GET method')
+})
+
+//post method - can be tested in postman alone
+app.post('/', (req,res) =>{
+    res.json({message:'Hello from post method',method:'post'})
+})
+
+/*
+Notes:
+1. express gives us more methods to handle simple tasks
+    - json method
+    - auomatic content type header...
+ */
